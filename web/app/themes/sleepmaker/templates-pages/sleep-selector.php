f2c4fe36a977b64b93a6d41008ea31b0c873f2c2
Nuwinda Udugala <nuwinda@overdose.digital>
2020-05-06 12:13:58 +1200

    selector page

--- web/app/themes/sleepmaker/templates-pages/sleep-selector.php
+++ web/app/themes/sleepmaker/templates-pages/sleep-selector.php
@@ -10,18 +10,17 @@
 
     <!-- These files always load from our main server-->
 
-    <script src="https://selector.thecomfortgroup.co/pub/media/js/sleepmaker_serta.js"></script>
-    <script src="https://selector.thecomfortgroup.co/pub/media/js/sleepsaas_serta.js"></script>
+    <script src="https://selector.thecomfortgroup.co/pub/media/js/sleepmaker_sleepmakernz.js"></script>
+    <script src="https://selector.thecomfortgroup.co/pub/media/js/sleepsaas_sleepyheadau.js"></script>
     <script src="https://selector.thecomfortgroup.co/pub/media/js/main.js"></script>
 
     <!-- These files always load from the local web site-->
-    <link rel="stylesheet" href="/assets/css/vendor.css">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="app/themes/sleepmaker/style.css?ver=1587461522">
     <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
 
     <div id="tcg_selector_container" style="margin: 0px !important;"></div>
     <script>
-        tcg_init_selector('e54496e8be9520c2a82855a55520af71', 1);
+        tcg_init_selector('09b419a7af87a518e4fe6ed939340597', 1);
 
     </script>
 <?php get_footer();
