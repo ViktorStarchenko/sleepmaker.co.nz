de43981e869e674efb5e00c1a484395137bb15f5
lin tom <linzw07@gmail.com>
2020-12-10 12:26:07 +1300

    Revert "change main site color"
    This reverts commit 3ffffd2bf9ab46517dfbe0b1399ead6b3316f805.

--- web/app/themes/sleepmaker/templates-parts/selectorWarranty.php
+++ web/app/themes/sleepmaker/templates-parts/selectorWarranty.php
@@ -6,7 +6,7 @@
     <div class="wrap-in">
         <div class="long-card-wrap">
             <div class="long-card">
-                <div class="long-card__info" style="background:#308ba1; color:#ffffff">
+                <div class="long-card__info" style="background:#002e5d; color:#ffffff">
                     <div class="long-card__info-inner">
                         <div class="long-card__title"><?= $warrantyBlock['first_title'] ?></div>
                         <p><?= $warrantyBlock['first_description'] ?></p><a class="bttn bttn--inverse" href="<?= $warrantyBlock['first_button']['link'] ?>"><?= $warrantyBlock['first_button']['label'] ?></a>
@@ -15,7 +15,7 @@
                 <div class="long-card__image"><img src="<?= $warrantyBlock['first_bg_image']['url'] ?>" alt="long-img-1"/></div>
             </div>
             <div class="long-card">
-                <div class="long-card__info" style="background:#308ba1; color:#ffffff">
+                <div class="long-card__info" style="background:#002e5d; color:#ffffff">
                     <div class="long-card__info-inner">
                         <div class="long-card__title"><?= $warrantyBlock['second_title'] ?></div>
                         <p><?= $warrantyBlock['second_description'] ?></p><a class="bttn bttn--inverse" href="<?= $warrantyBlock['second_button']['link'] ?>"><?= $warrantyBlock['second_button']['label'] ?></a>
